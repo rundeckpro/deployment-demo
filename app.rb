e43c5faacbc3322382e4bbcddde63979393823c4
chobbs <craig.hobbs@gmail.com>
2020-12-01 21:20:08 -0800

    fix: welcome msg

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 
 
   get '/' do
-    "Rundeck Deployment Test App<br />Hi Smartsheet, It's Tuesday!"
+    "Rundeck Deployment Test App<br />Hi JAson, It's Wednesday!"
   end
 
   get '/testval' do
